0dec6d82c9da73cad81549bf6a8cce0ed70a687b
Chloe Vander Zwan <chloe@transparentclassroom.com>
2025-03-03 17:46:08 -0800

    third commit

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/* pseudocode:
+1. Create a string that says "You have received this message because you have been chosen to open an important vault. Here is the secret combination";
+2. Make three varialbles the result of a unique operator, easch one is part of the vault code 10-40-39
+3. Add comments in the code
+4. Create a dialogue box that displays the vault code */
+
 const message =
   "You have received this message because you have been chosen to open an important vault. Here is the secret combination";
 //use addition to get the first number
