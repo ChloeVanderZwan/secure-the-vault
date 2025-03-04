38a7034168f4533d1fe803c8bfd8db014fdfab93
Chloe Vander Zwan <chloe@transparentclassroom.com>
2025-03-03 17:40:05 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const message =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination";
+const num1 = 9 + 1;
+const num2 = 80 / 2;
+const num3 = 40 - 1;
+alert(`${message}: ${num1} - ${num2} - ${num3}`);
